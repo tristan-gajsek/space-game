4810e736836046c046876408a419b16a65636cb8
dynamo <zigagorenc1@gmail.com>
2024-05-12 14:18:09 +0200

    Added basic laser collision detection

--- Scripts/Players/player.gd
+++ Scripts/Players/player.gd
@@ -1,4 +1,11 @@
 extends CharacterBody2D
 
+@onready var laser_1 = $"Laser 1"
+@onready var laser_2 = $"Laser 2"
+
 func _physics_process(delta):
 	position.x = get_global_mouse_position().x
+
+func _process(delta):
+	if laser_1.is_colliding() or laser_2.is_colliding():
+		print("Hit!")
